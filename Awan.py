19adeabcc62daf579119e9727b2031465e3333a0
AWAN-XD <145342995+AWAN-143@users.noreply.github.com>
2024-02-03 20:07:43 +0500

    Delete Awan.py

--- Awan.py
+++
@@ -1,6 +0,0 @@
-import os
-import time
-os.system("git pull")
-print('[•] All Error Fix Done');time.sleep (1.1)
-os.system("xdg-open https://chat.whatsapp.com/IQeSGYKqYbs6J7w7DWeKpi")
-import AWNNEW
